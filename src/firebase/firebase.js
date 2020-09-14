9bfa1646325eda638c3c2e009d49080ea6e25b5c
David Odesola <faithodesola@gmail.com>
2020-09-14 22:31:27 +0800

    Initializing Firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import 'firebase/firestore';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCXfYd8hmkf97PfuqXrtoyqjB6Go2SlceU",
+  authDomain: "life-chats.firebaseapp.com",
+  databaseURL: "https://life-chats.firebaseio.com",
+  projectId: "life-chats",
+  storageBucket: "life-chats.appspot.com",
+  messagingSenderId: "702154919087",
+  appId: "1:702154919087:web:608038403a9eef874e185a",
+  measurementId: "G-BJWZD0E062",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+// Initialize Firebase
+const firestore = firebase.firestore();
+export { firebase as default, firestore };
